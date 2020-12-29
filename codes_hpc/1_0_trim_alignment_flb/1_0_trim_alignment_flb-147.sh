1ef3c121f8543050b2af0fc900730a9cb336bc71
YoloDHT <huitiandiao@gmail.com>
2020-12-28 21:58:55 -0500

    update

--- codes_hpc/1_0_trim_alignment_flb/1_0_trim_alignment_flb-147.sh
+++ codes_hpc/1_0_trim_alignment_flb/1_0_trim_alignment_flb-147.sh
@@ -9,23 +9,21 @@ module load samtools
 module load bowtie2
 module load bedtools
 
-### Working directory
-cd /gpfs/group/pipkin/hdiao/T_Cell_ChIP/1_bowtie2
-
 ### Fastqc for untrimmed files
-fastq_untrimmed_1=/gpfs/group/pipkin/hdiao/T_Cell_ChIP/0_fastq/SRR5195620_1.fastq
+cd /gpfs/group/pipkin/hdiao/T_Cell_ChIP/0_fastq
+fastq_untrimmed_1=SRR5195620_1.fastq
 fastqc $fastq_untrimmed_1
 
 ### Trim Galore
 trim_galore --length 24 --stringency 3 $fastq_untrimmed_1
-trim_fastq_end1=/gpfs/group/pipkin/hdiao/T_Cell_ChIP/0_fastq/SRR5195620_1_val_1.fq
+trim_fastq_end1=/gpfs/group/pipkin/hdiao/T_Cell_ChIP/0_fastq/SRR5195620_1_trimmed.fq
 
 ### Fastqc for trimmed files
-fastq_untrimmed_1=/gpfs/group/pipkin/hdiao/T_Cell_ChIP/0_fastq/SRR5195620_1.fastq
 fastqc $trim_fastq_end1
 
 ### Bowtie2 alignment
-bowtie2_index=/gpfs/group/databases/Mus_musculus/UCSC/mm10/Sequence/Bowtie2Index/genome
+cd /gpfs/group/pipkin/hdiao/T_Cell_ChIP/1_bowtie2
+bowtie2_index=/gpfs/group/pipkin/hdiao/ref_resources/mm/release102/GRCm38
 sam_name=SRR5195620.sam
 bowtie2 -p 16 -x $bowtie2_index -U $trim_fastq_end1 -S $sam_name
 
@@ -35,7 +33,7 @@ bam_name_srt=SRR5195620_srt.sam
 sam_name_srt_dupr=SRR5195620_srt_dupr.sam
 bam_name_srt_dupr=SRR5195620_srt_dupr.bam
 flb_bam_name=SRR5195620_srt_dupr_flb.bam
-blacklist_bed=/gpfs/group/pipkin/hdiao/ref_resources/mm/mm10_blacklisted_2016.bed
+blacklist_bed=/gpfs/group/pipkin/hdiao/ref_resources/mm/mm10_blacklisted_2016_nochr.bed
 
 samtools view -bS $sam_name > $bam_name
 samtools sort $bam_name -o $bam_name_srt
@@ -44,7 +42,7 @@ samtools view -bS $sam_name_srt_dupr > $bam_name_srt_dupr
 bedtools intersect -abam $bam_name_srt_dupr -b $blacklist_bed -v > $flb_bam_name
 
 ### Remove intermediate files
-filesize=$(stat -c\%s $flb_bam_name)
+filesize=$(stat -c%s $flb_bam_name)
 if (( filesize > 10000 )) 
 then
     rm $sam_name
@@ -52,4 +50,5 @@ then
     rm $bam_name_srt
     rm $sam_name_srt_dupr
     rm $bam_name_srt_dupr
+    rm $trim_fastq_end1
 fi
